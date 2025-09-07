56b7ae9628eec2303c18f5ada740f49cb820cbe1
Oviyasaravanan <oviya5392@gmail.com>
2025-09-07 12:29:48 +0530

    Added my first code

---
+++ ov4.py
@@ -0,0 +1,14 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operation = input("Enter +, -, *, /: ")
+
+if operation == '+':
+    print("Result:", num1 + num2)
+elif operation == '-':
+    print("Result:", num1 - num2)
+elif operation == '*':
+    print("Result:", num1 * num2)
+elif operation == '/':
+    print("Result:", num1 / num2)
+else:
+    print("Invalid operation")
